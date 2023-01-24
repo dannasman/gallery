19a4850c1d240b81ffec57a8b353b47312ef100d
Dan Nasman <dan.nasman@gmail.com>
2023-01-24 21:13:26 +0200

    rotate birdz

--- gallery/sunset.js
+++ gallery/sunset.js
@@ -19,80 +19,38 @@ function draw() {
     arc(0, i*5, 650+2000*i/arc_interval, 200, 0, PI)
   }
   
-  let birdsX = 200
-  let birdsY = -200
+  let birdsXs = [200, -200, 0]
+  let birdsYs = [-200, -300, -350]
 
-  for(let i = 0; i < 3; i++) {
-    if (i != 1) {
-      fill(0);
-      noStroke()
-      beginShape();
-      vertex(birdsX+i*10, birdsY-4);
-      vertex(birdsX+6+i*10, birdsY);
-      vertex(birdsX+i*10, birdsY-6);
-      vertex(birdsX-6+i*10, birdsY);
-      endShape();
-    } else {
-      fill(0);
-      noStroke()
-      beginShape();
-      vertex(birdsX+i*10, birdsY-24);
-      vertex(birdsX+6+i*10, birdsY-20);
-      vertex(birdsX+i*10, birdsY-26);
-      vertex(birdsX+-6+i*10, birdsY-20);
-      endShape();
-    }
-  }
+  let angles = [PI/6, -PI/18, PI/9]
 
-  birdsX = -200
-  birdsY = -300
-
-  for(let i = 0; i < 3; i++) {
-    if (i != 1) {
-      fill(0);
-      noStroke()
-      beginShape();
-      vertex(birdsX+i*10, birdsY-4);
-      vertex(birdsX+6+i*10, birdsY);
-      vertex(birdsX+i*10, birdsY-6);
-      vertex(birdsX-6+i*10, birdsY);
-      endShape();
-    } else {
-      fill(0);
-      noStroke()
-      beginShape();
-      vertex(birdsX+i*10, birdsY-24);
-      vertex(birdsX+6+i*10, birdsY-20);
-      vertex(birdsX+i*10, birdsY-26);
-      vertex(birdsX+-6+i*10, birdsY-20);
-      endShape();
-    }
-  }
-
-  birdsX = 0
-  birdsY = -350
-  rotation1 = Math.cos(PI/9)
-  rotation2 = Math.sin(PI/9)
-
-  for(let i = 0; i < 3; i++) {
-    if (i != 1) {
-      fill(0);
-      noStroke()
-      beginShape();
-      vertex(birdsX+i*10*rotation1+4*rotation2, birdsY+i*10*rotation2-4*rotation1);
-      vertex(birdsX+(6+i*10)*rotation1, birdsY+(6+i*10)*rotation2);
-      vertex(birdsX+i*10*rotation1+6*rotation2, birdsY+i*10*rotation2-6*rotation1);
-      vertex(birdsX+(-6+i*10)*rotation1, birdsY+(-6+i*10)*rotation2);
-      endShape();
-    } else {
-      fill(0);
-      noStroke()
-      beginShape();
-      vertex(birdsX+i*10*rotation1+24*rotation2, birdsY+i*10*rotation2-24*rotation1);
-      vertex(birdsX+(6+i*10)*rotation1+20*rotation2, birdsY+(6+i*10)*rotation2-20*rotation1);
-      vertex(birdsX+i*10*rotation1+26*rotation2, birdsY+i*10*rotation2-26*rotation1);
-      vertex(birdsX+(-6+i*10)*rotation1+20*rotation2, birdsY+(-6+i*10)*rotation2-20*rotation1);
-      endShape();
+  for(let n = 0; n < 3; n++) {
+    birdsX = birdsXs[n]
+    birdsY = birdsYs[n]
+    
+    rotation1 = Math.cos(angles[n])
+    rotation2 = Math.sin(angles[n])
+    
+    for(let i = 0; i < 3; i++) {
+      if (i != 1) {
+        fill(0);
+        noStroke()
+        beginShape();
+        vertex(birdsX+i*10*rotation1+4*rotation2, birdsY+i*10*rotation2-4*rotation1);
+        vertex(birdsX+(6+i*10)*rotation1, birdsY+(6+i*10)*rotation2);
+        vertex(birdsX+i*10*rotation1+6*rotation2, birdsY+i*10*rotation2-6*rotation1);
+        vertex(birdsX+(-6+i*10)*rotation1, birdsY+(-6+i*10)*rotation2);
+        endShape();
+      } else {
+        fill(0);
+        noStroke()
+        beginShape();
+        vertex(birdsX+i*10*rotation1+24*rotation2, birdsY+i*10*rotation2-24*rotation1);
+        vertex(birdsX+(6+i*10)*rotation1+20*rotation2, birdsY+(6+i*10)*rotation2-20*rotation1);
+        vertex(birdsX+i*10*rotation1+26*rotation2, birdsY+i*10*rotation2-26*rotation1);
+        vertex(birdsX+(-6+i*10)*rotation1+20*rotation2, birdsY+(-6+i*10)*rotation2-20*rotation1);
+        endShape();
+      }
     }
   }
 }
